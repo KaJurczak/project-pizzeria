7f3f0fdb6e4fe19c02982f5a096607c1a2be1238
Karolina Jurczak <inka_c@tlen.pl>
2020-01-19 22:20:45 +0100

    Split file script and another, withougt bug

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,6 +1,7 @@
-import {select, classNames, templates} from './settings.js';
-import utils from './utils.js';
-import AmountWidget from './components/AmountWidget.js';
+import {select, classNames, templates} from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
+
 
 class Product {
   constructor(id, data){
